66afe5ad094d5512c8549ab6c7032c97d53b2f3d
Joseph Kemper <JosephKemper@gmail.com>
2023-09-16 18:06:26 -0600

    Testing Commit in IntelliJ

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>) {
     println("Hello World! I'm playing with Intellij")
+    println("Hello World! Testing github interaction in Intellij")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
